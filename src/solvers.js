df8a727cc026c2e4876d9fbb2e6a712d81054906
Hack Reactor Students <github@hackreactor.com>
2013-10-02 21:28:55 -0700

    First attempt at N Queens

--- src/solvers.js
+++ src/solvers.js
@@ -41,8 +41,6 @@ window.findNRooksSolution = function(n){
     }
       return false;
   }
-
-
   return newRows;
 };
 
@@ -51,30 +49,15 @@ window.findNRooksSolution = function(n){
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n){
   var results = [];
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-
+  if (n === 0 || n === 1) return 1;
   subroutine();
-
-
-  function contains (list, value) {
-    for (var i = 0; i < list.length; i++) {
-      if (list[i] === value) {
-        return true;
-      }
-    }
-    return false;
-  }
 
   function subroutine (invalid, row, board) {
     invalid = invalid || [];
     row = row || 0;
     board = board || new Board({n:n}).rows();
-    if (row === n) {
-      results.push(board);
-      return;
-    }
+
+    if (row === n) return results.push(board);
 
     for (var column = 0; column < n; column++) {
       if (!_.contains(invalid, column)){
@@ -89,42 +72,71 @@ window.countNRooksSolutions = function(n){
 };
 
 
-/*function nrooks (n)
-
-var results = [];
-
-function recursive (invalid_columns, round, board){
-  round = round || 0
-  board = board || new Board({n : n})
-  invalid_columns = invalid_columns || [];
-}
-
-if round = 0 {
-results.push(board);
-return;
-}
-
-  loop through all elements in our current row
-    if the index isn't in invalid columns array
-      make a new copy of the board with a piece added at our current position
-      var newboard = board
-      new board row whatever column something = 1
-      make a new copy of invalid_columns with the current column (our current index) added to it
-      return the function with new board copy, new invalid_columns copy, and round - 1 as arguments
-
-}
-
-return results
-
-*/
-
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
+  var solution = new Board({n:n});
+  var newRows = solution.rows();
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  for (var row = 0; row < newRows.length; row++) {
+    for (var i = 0; i < newRows[row].length; i++) {
+      newRows[row][i] = 1;
+      if(hasColConflictAt(i) || hasMajorDiagonalConflictAt(i) || hasMinorDiagonalConflictAt(i)){
+        newRows[row][i] = 0;
+      } else {
+        break;
+      }
+    }
+  }
+
+  function hasColConflictAt(colIndex){
+    var onesFound = 0;
+    var allRows = newRows;
+
+    for (var i = 0; i < allRows.length; i++) {
+      if (allRows[i][colIndex] === 1) {
+        onesFound++;
+      }
+    }
+    if (onesFound > 1) {
+      return true;
+    }
+      return false;
+  }
+
+  function hasMajorDiagonalConflictAt (majorDiagonalColumnIndexAtFirstRow){
+    var piecesFound = 0;
+    var theRows = newRows;
+
+    for (var i = 0; i < theRows.length; i++) {
+      for (var j = 0; j < theRows[i].length; j++) {
+        if ((j - i) === majorDiagonalColumnIndexAtFirstRow && theRows[i][j] === 1) {
+          piecesFound++;
+        }
+      }
+    }
+    if (piecesFound > 1) {
+      return true;
+    }
+    return false;
+  }
+
+  function hasMinorDiagonalConflictAt (minorDiagonalColumnIndexAtFirstRow){
+    var piecesFound = 0;
+    var theRows = newRows;
+
+    for (var i = 0; i < theRows.length; i++) {
+      for (var j = 0; j < theRows[i].length; j++) {
+        if ((j + i) === minorDiagonalColumnIndexAtFirstRow && theRows[i][j] === 1) {
+          piecesFound++;
+        }
+      }
+    }
+    if (piecesFound > 1) {
+      return true;
+    }
+    return false;
+  }
+  return newRows;
 };
 
 
